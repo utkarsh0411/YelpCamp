2edb23774bc5d5be6dabebe5994959837863ceb2
Utkarsh Singh <utkarsh0411@gmail.com>
2019-12-30 23:40:45 +0530

    "Removed unrequired files"

--- models/campgrounds.js
+++
@@ -1,9 +0,0 @@
-var mongoose=require('mongoose');
-
-var campSchema=new mongoose.Schema({
-    name:String,
-    image:String,
-    description:String
-});
-
-module.exports=mongoose.model("Campgrounds",campSchema);
