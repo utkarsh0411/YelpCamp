3d12f5959708fe3f9c785bd461e27e740ba0cc0b
Utkarsh Singh <utkarsh0411@gmail.com>
2020-03-11 23:36:53 +0530

    "Search feature for campground added"

---
+++ v13/models/campgrounds.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+
+var campSchema = new mongoose.Schema({
+    name: String,
+    price:String,
+    image: String,
+    description: String,
+    location:String,
+    lat:Number,
+    lng:Number,
+    created:{type: Date, default:Date.now},
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    ],
+    creator: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        username:String
+    }
+});
+
+module.exports = mongoose.model("Campgrounds", campSchema);
